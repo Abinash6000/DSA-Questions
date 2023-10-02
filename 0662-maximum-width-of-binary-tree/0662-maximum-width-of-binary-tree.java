61f651a8383a1d19cb87b41bbd3e83c95128aca5
Abinash <110354418+Abinash6000@users.noreply.github.com>
2023-10-02 15:49:04 +0530

    Time: 1 ms (99.2%), Space: 43.1 MB (53.64%) - LeetHub

---
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        Queue<Tuple> q = new LinkedList<>();
+        q.add(new Tuple(1, 0, root)); // num, level, node
+        int prevNum = 1, prevLevel = 0;
+        int res = 0;
+
+        while(!q.isEmpty()) {
+            Tuple curr = q.remove();
+            int num = curr.num, level = curr.level;
+            TreeNode node = curr.node;
+
+            if(prevLevel < level) {
+                prevLevel = level;
+                prevNum = num;
+            }
+
+            res = Math.max(res, num - prevNum +1);
+
+            if(node.left != null) {
+                q.add(new Tuple(num*2, level+1, node.left));
+            } if(node.right != null) {
+                q.add(new Tuple(num*2+1, level+1, node.right));
+            }
+        }
+
+        return res;
+    }
+
+    class Tuple {
+        int num;
+        int level;
+        TreeNode node;
+
+        Tuple(int num, int level, TreeNode node) {
+            this.num = num;
+            this.level = level;
+            this.node = node;
+        }
+    }
+}
