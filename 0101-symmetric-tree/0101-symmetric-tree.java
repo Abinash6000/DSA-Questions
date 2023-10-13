faa44214a4bad024ffa79bfac4226ca506921309
Abinash <110354418+Abinash6000@users.noreply.github.com>
2023-10-13 08:57:59 +0530

    Time: 0 ms (100%), Space: 40.7 MB (28.9%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return helper(root.left, root.right);
+    }
+
+    private boolean helper(TreeNode left, TreeNode right) {
+
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+        if(left.val != right.val) return false;
+
+        boolean ans = helper(left.left, right.right) && helper(left.right, right.left);
+        return ans;
+    }
+}
