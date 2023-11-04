3e08bbd18ca86bd319a7870e561ed741b7f60737
Abinash <110354418+Abinash6000@users.noreply.github.com>
2023-11-04 05:57:28 +0530

    Time: 0 ms (100%), Space: 44.5 MB (7.9%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,12 +1,23 @@
 class Solution {
+    // We use Integer instead of int as it supports a null value.
+    private Integer prev;
+
     public boolean isValidBST(TreeNode root) {
-        return validate(root, null, null);
+        prev = null;
+        return inorder(root);
     }
-    private boolean validate(TreeNode root, Integer low, Integer high) {
-        if(root==null) return true;
 
-        if((low!=null && root.val<=low) || (high!=null && root.val>=high)) return false;
-
-        return validate(root.left, low, root.val) && validate(root.right, root.val, high);
+    private boolean inorder(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (!inorder(root.left)) {
+            return false;
+        }
+        if (prev != null && root.val <= prev) {
+            return false;
+        }
+        prev = root.val;
+        return inorder(root.right);
     }
 }
