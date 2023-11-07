4e14aca0e5bf4a5c300a6164b913ab9402c2b67b
Abinash <110354418+Abinash6000@users.noreply.github.com>
2023-11-07 15:32:23 +0530

    Time: 5 ms (100%), Space: 43.8 MB (68.62%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,28 +1,12 @@
 class Solution {
-    TreeNode ans = new TreeNode();
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        helper(root, p, q);
-        return ans;
-    }
-
-    boolean helper(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null) return false;
-        
-        boolean left = helper(root.left, p, q);
-        boolean right = helper(root.right, p, q);
-
-        if(left && right) {
-            ans = root;
-            return false;
-        } else if((left || right) && (root.val == p.val || root.val == q.val)) {
-            ans = root;
-            return false;
-        } else if(left || right) {
-            return true;
-        } else if(root.val == p.val || root.val == q.val) { // the left and right is false here
-            return true;
-        } else { // the left and right and root are false
-            return false;
-        }
+        if((p.val > root.val && q.val < root.val) || (p.val < root.val && q.val > root.val)) 
+            return root;
+        else if(p.val == root.val || q.val == root.val) 
+            return root;
+        else if(p.val > root.val) 
+            return lowestCommonAncestor(root.right, p, q);
+        else 
+            return lowestCommonAncestor(root.left, p, q);
     }
 }
