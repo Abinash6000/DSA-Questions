a0d1461178932f95f36890206079b32b112cf508
Abinash <110354418+Abinash6000@users.noreply.github.com>
2023-11-08 19:31:11 +0530

    Time: 1 ms (42.04%), Space: 44 MB (44.07%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -1,21 +1,23 @@
 class Solution {
-    int ans;
-    int k;
     public int kthSmallest(TreeNode root, int k) {
-        ans = -1;
-        this.k = k;
-        helper(root);
-        return ans;
-    }
-
-    private void helper(TreeNode root) {
-        if(root == null) return;
-
-        helper(root.left);
-        if(k == 1) ans = root.val;
-        k--;
-
-        helper(root.right);
-
+        Stack<TreeNode> st = new Stack<>();
+        
+        while (root != null) {
+            st.push(root);
+            root = root.left;
+        }
+            
+        while (k != 0) {
+            TreeNode n = st.pop();
+            k--;
+            if (k == 0) return n.val;
+            TreeNode right = n.right;
+            while (right != null) {
+                st.push(right);
+                right = right.left;
+            }
+        }
+        
+        return -1; // never hit if k is valid
     }
 }
