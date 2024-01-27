87e715c9351bc9eeca2176679b02368fc4064ac3
Abinash <110354418+Abinash6000@users.noreply.github.com>
2024-01-27 22:36:09 +0530

    Time: 14 ms (87.64%), Space: 44.3 MB (44.83%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,21 +1,30 @@
 class Solution {
     int memo[][];
     public int coinChange(int[] coins, int amount) {
-        memo = new int[10001][coins.length];
-        int ans = solve(coins, amount, 0);
-        return ans == 10001 ? -1 : ans;
-    }
+        int dp[] = new int[amount+1];
+        for(int i = 0; i<amount+1; i++) dp[i] = amount+1;
+        dp[0] = 0;
 
-    private int solve(int[] coins, int amt, int i) {
-        if(amt == 0) return 0;
-        if(i == coins.length) return 10001;
-        if(memo[amt][i] != 0) return memo[amt][i];
-
-        if(coins[i] <= amt) {
-            // two choices take/not take
-            return memo[amt][i] = Math.min(1 + solve(coins, amt-coins[i], i), solve(coins, amt, i+1));
-        } else {
-            return memo[amt][i] = solve(coins, amt, i+1);
+        for(int a = 1; a<amount+1; a++) {
+            for(int c : coins) {
+                if(a>=c)
+                    dp[a] = Math.min(dp[a], 1 + dp[a-c]);
+            }
         }
+
+        return  dp[amount] != amount+1 ? dp[amount] : -1;
     }
+
+    // private int solve(int[] coins, int amt, int i) {
+    //     if(amt == 0) return 0;
+    //     if(i == coins.length) return 10001;
+    //     if(memo[amt][i] != 0) return memo[amt][i];
+
+    //     if(coins[i] <= amt) {
+    //         // two choices take/not take
+    //         return memo[amt][i] = Math.min(1 + solve(coins, amt-coins[i], i), solve(coins, amt, i+1));
+    //     } else {
+    //         return memo[amt][i] = solve(coins, amt, i+1);
+    //     }
+    // }
 }
