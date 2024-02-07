056321e5c509ee67a277d11592fc9a78dd984d80
Abinash <110354418+Abinash6000@users.noreply.github.com>
2024-02-07 06:12:46 +0530

    Time: 306 ms (5.35%), Space: 47.1 MB (6.81%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         HashMap<String, Boolean> hm = new HashMap<>();
-        return helper(s, wordDict, hm);
+        Set<String> set = new HashSet<>(wordDict);
+        return helper(s, set, hm);
     }
 
-    private boolean helper(String s, List<String> wordDict, HashMap<String, Boolean> hm) {
-        if(wordDict.contains(s)) return true;
+    private boolean helper(String s, Set<String> set, HashMap<String, Boolean> hm) {
+        if(set.contains(s)) return true;
         if(hm.containsKey(s)) return hm.get(s);
 
         for(int i = 1; i<s.length(); i++) {
             // divide the string into two parts...
-            if(helper(s.substring(0,i), wordDict, hm) && helper(s.substring(i, s.length()), wordDict, hm)) {
+            if(helper(s.substring(0,i), set, hm) && helper(s.substring(i, s.length()), set, hm)) {
                 hm.put(s, true);
                 return true;
             }
