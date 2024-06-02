ae60eaa2d9c59bacff39125a1eb88f77840470fd
Abinash <110354418+Abinash6000@users.noreply.github.com>
2024-06-02 09:59:15 +0530

    Time: 1 ms (11.47%), Space: 40.4 MB (38.63%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,14 +1,24 @@
 class Solution {
     public int uniquePaths(int r, int c) {
-        Integer memo[][] = new Integer[r+1][c+1];
-        return up(r, c, memo);
+        Integer memo[][] = new Integer[r][c];
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                if(i == 0 || j == 0) {
+                    memo[i][j] = 1;
+                    continue;
+                }
+                memo[i][j] = memo[i-1][j] + memo[i][j-1];
+            }
+        }
+
+        return memo[r-1][c-1];
     }
 
-    private int up(int r, int c, Integer[][] memo) {
-        if(r == 0 || c == 0) return 0;
-        if(r == 1 && c == 1) return 1;
-        if(memo[r][c] != null) return memo[r][c];
+    // private int up(int r, int c, Integer[][] memo) {
+    //     if(r == 0 || c == 0) return 0;
+    //     if(r == 1 && c == 1) return 1;
+    //     if(memo[r][c] != null) return memo[r][c];
 
-        return memo[r][c] = (up(r-1, c, memo) + up(r, c-1, memo));
-    }
+    //     return memo[r][c] = (up(r-1, c, memo) + up(r, c-1, memo));
+    // }
 }
