1fe0590d7ba8375fa2792cf47a0208e749a96536
Abinash <110354418+Abinash6000@users.noreply.github.com>
2024-06-02 08:50:20 +0530

    Time: 2 ms (99.31%), Space: 47.3 MB (88.74%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,39 +1,31 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> ans = new ArrayList<>();
-        if(nums.length == 1) {
-            ans.add(nums[0]);
-            return ans;
-        }
-
-        int count1 =  0, count2 = 0;
-        int candidate1 = -1, candidate2 = -2;
-
-        for(int e : nums) {
-            if(count1 <= 0) {
-                candidate1 = e;
+        List<Integer> l = new ArrayList<>();
+        int count1 = 0, count2 = 0, candidate1 = 0, candidate2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == candidate1) {
                 count1++;
-            } else if(count2 <= 0) {
-                candidate2 = e;
+            } else if (nums[i] == candidate2) {
                 count2++;
-            }
-
-            if(e == candidate1) count1++;
-            else if(e == candidate2) count2++;
-            else {
-                count1--; count2--;
+            } else if (count1 == 0) {
+                count1 = 1;
+                candidate1 = nums[i];
+            } else if (count2 == 0) {
+                count2 = 1;
+                candidate2 = nums[i];
+            } else {
+                count1--;
+                count2--;
             }
         }
-
-        int c1 = 0, c2 = 0;
-        for(int e : nums) {
-            if(e == candidate1) c1++;
-            else if(e == candidate2) c2++;
+        count1 = 0; 
+        count2 = 0;
+        for (int num : nums) {
+            if(num==candidate1) count1++;
+            else if(num==candidate2) count2++;
         }
-        
-        if(c1 > nums.length/3) ans.add(candidate1);
-        if(c2 > nums.length/3) ans.add(candidate2);
-
-        return ans;
+        if(count1>nums.length/3) l.add(candidate1);
+        if(count2>nums.length/3) l.add(candidate2);
+        return l;
     }
 }
