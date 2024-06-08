5a1ff30cd7b3568a95353c5521ed14ec6b46c2ee
Abinash <110354418+Abinash6000@users.noreply.github.com>
2024-06-08 14:18:19 +0530

    Time: 1 ms (99.92%), Space: 43.9 MB (98.03%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(candidates);
-        helper(candidates, target, 0, new ArrayList<>(), res);
-        return res;
+        List<List<Integer>> combs = new ArrayList<>();
+        uniqueComb(candidates, target, combs, new ArrayList<>(), 0);
+        return combs;
     }
 
-    private void helper(int[] candidates, int target, int pos, List<Integer> currComb, List<List<Integer>> res) {
+    private void uniqueComb(int[] arr, int target, List<List<Integer>> combs, List<Integer> comb, int idx) {
         if(target == 0) {
-            res.add(new ArrayList<>(currComb));
+            combs.add(new ArrayList(comb));
             return;
         }
-        if(target < 0) {
-            return;
-        }
+        if(idx == arr.length) return;
 
-        for(int i = pos; i<candidates.length; i++) {
-            currComb.add(candidates[i]);
-            helper(candidates, target-candidates[i], i, currComb, res);
-            currComb.remove(currComb.size()-1);
+        if(arr[idx] <= target) {
+            comb.add(arr[idx]);
+            uniqueComb(arr, target-arr[idx], combs, comb, idx);
+            comb.remove(comb.size()-1);
         }
+        uniqueComb(arr, target, combs, comb, idx+1);
     }
 }
