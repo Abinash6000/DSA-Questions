50889873650af7fd07d20f2c2ef4eba6dab5568d
Abinash <110354418+Abinash6000@users.noreply.github.com>
2024-07-27 18:42:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -3,17 +3,19 @@ class Solution {
         int n = nums.length;
         if(nums[0] < nums[n-1] || n == 1) return nums[0];
 
-        int st = 0, ed = n-1, mid;
+        int st = 0, ed = n-1, min = 5001, mid;
         
         while(st <= ed) {
             mid = st + (ed-st)/2;
-            if(mid != 0 && nums[mid]<nums[mid-1]) return nums[mid];
-            else if(nums[0] <= nums[mid]) // left side sorted
+            if(nums[st] < nums[mid]) {// left side sorted, take min from left side
+                min = Math.min(min, nums[st]);
                 st = mid+1;
-            else // right side sorted
+            } else { // right side sorted, take min from right side
+                min = Math.min(min, nums[mid]);
                 ed = mid-1;
+            }
         }
 
-        return -1;
+        return min;
     }
 }
