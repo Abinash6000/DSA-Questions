2aafd4033758de44dcda2c858e7d5ea5c041ba39
Abinash <110354418+Abinash6000@users.noreply.github.com>
2024-09-01 07:02:07 +0530

    Time: 2 ms (63.07%), Space: 46.1 MB (40.76%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -5,24 +5,24 @@ class Solution {
         int low = 0, high = n1;
 
         while(low <= high) {
-            int cut1 = low + (high - low)/2;
-            int cut2 = (n1 + n2 + 1)/2 - cut1;
+            int mid1 = low + (high - low)/2; // x -> the no. of elements I take from A1
+            int mid2 = (n1 + n2 + 1)/2 - mid1; // because I need to take (real mid (median point) - mid1); this gives me no. of elements I can take from A2
 
-            int left1 = cut1 == 0 ? Integer.MIN_VALUE : nums1[cut1-1];
-            int left2 = cut2 == 0 ? Integer.MIN_VALUE : nums2[cut2-1];
+            int l1 = mid1 == 0 ? Integer.MIN_VALUE : nums1[mid1-1]; // if mid1 is 0 that means no elements are taken from A1, so minimum value
+            int l2 = mid2 == 0 ? Integer.MIN_VALUE : nums2[mid2-1]; // if mid2 is 0 that means no elements are taken from A2
 
-            int right1 = cut1 == n1 ? Integer.MAX_VALUE : nums1[cut1];
-            int right2 = cut2 == n2 ? Integer.MAX_VALUE : nums2[cut2];
+            int r1 = mid1 == n1 ? Integer.MAX_VALUE : nums1[mid1];
+            int r2 = mid2 == n2 ? Integer.MAX_VALUE : nums2[mid2];
 
-            if(left1 <= right2 && left2 <= right1) {
+            if(l1 <= r2 && l2 <= r1) {
                 if((n1 + n2)%2 == 1) 
-                    return Math.max(left1, left2);
+                    return Math.max(l1, l2);
                 else 
-                    return (Math.max(left1, left2) + Math.min(right1, right2))/2.0;
-            } else if(left1 > right2) {
-                high = cut1-1;
+                    return (Math.max(l1, l2) + Math.min(r1, r2))/2.0;
+            } else if(l1 > r2) {
+                high = mid1-1;
             } else {
-                low = cut1+1;
+                low = mid1+1;
             }
         }
 
