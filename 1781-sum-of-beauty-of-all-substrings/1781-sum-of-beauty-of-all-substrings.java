febac6c7d05756b34accdd1f642755c9473f8d67
Abinash <110354418+Abinash6000@users.noreply.github.com>
2024-09-05 12:35:48 +0530

    Time: 75 ms (41.92%), Space: 42.4 MB (89.14%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i = 0; i<n; i++) {
             int[] freqCount = new int[26];
             freqCount[s.charAt(i)-'a']++;
-            min = 501; max = 0;
+            min = 1; max = 1;
 
             for(int j = i+1; j<n; j++) {
                 char currChar = s.charAt(j);
@@ -14,7 +14,11 @@ class Solution {
                 int currCount = freqCount[currChar-'a'];
 
                 if(currCount > max) max = currCount;
-                if(currCount < min) min = currCount;
+                min = 501;
+                for(int k = 0; k<26; k++) {
+                    if(freqCount[k] == 0) continue;
+                    if(freqCount[k] < min) min = freqCount[k];
+                }
                 sum += max-min;
             }
         }
