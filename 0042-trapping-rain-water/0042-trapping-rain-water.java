57880370227e1eeeb16b476e3567b15c4baaa354
Abinash <110354418+Abinash6000@users.noreply.github.com>
2025-04-04 09:03:13 +0530

    Time: 6 ms (7.09%), Space: 45.4 MB (98.58%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,23 +1,19 @@
-// https://www.youtube.com/watch?v=ZI2z5pq0TqA
 class Solution {
-    public int trap(int[] height) {
-        
-        int l = 0, r = height.length-1;
-        int leftMax = height[l], rightMax = height[r];
-        int water = 0;
-
-        while(l < r) {
-            if(leftMax < rightMax) {
-                l += 1;
-                leftMax = Math.max(leftMax, height[l]);
-                water += leftMax - height[l];
-            } else {
-                r -= 1;
-                rightMax = Math.max(rightMax, height[r]);
-                water += rightMax - height[r];
+    public int trap(int[] A) {
+        if (A==null) return 0;
+        Stack<Integer> s = new Stack<Integer>();
+        int i = 0, maxWater = 0, maxBotWater = 0;
+        while (i < A.length){
+            if (s.isEmpty() || A[i]<=A[s.peek()]){
+                s.push(i++);
+            }
+            else {
+                int bot = s.pop();
+                maxBotWater = s.isEmpty()? // empty means no il
+                0:(Math.min(A[s.peek()],A[i])-A[bot])*(i-s.peek()-1);
+                maxWater += maxBotWater;
             }
         }
-
-        return water;
+        return maxWater;
     }
 }
