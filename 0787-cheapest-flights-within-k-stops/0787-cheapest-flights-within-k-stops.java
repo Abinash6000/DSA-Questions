101cb3cc01f1b6162bfba24975df762144f29f60
Abinash <110354418+Abinash6000@users.noreply.github.com>
2025-08-24 19:43:33 +0530

    Time: 4 ms (99.33%), Space: 44.6 MB (34.61%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,30 +1,25 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<int[]>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
-        for (int[] f : flights) adj.get(f[0]).add(new int[]{f[1], f[2]});
+        final int INF = Integer.MAX_VALUE/2; // divide by 2 to just keep elements in range
+        int[] dist = new int[n];
 
-        int[][] dist = new int[n][k+2]; 
-        for (int[] d : dist) Arrays.fill(d, (int)1e9);
-        dist[src][0] = 0;
+        Arrays.fill(dist, INF);
+        dist[src] = 0;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
-        pq.add(new int[]{src, 0, 0}); // node, cost, stops
+        for(int i = 0; i<=k; i++) {
+            int[] temp = dist.clone();
 
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int node = cur[0], cost = cur[1], stops = cur[2];
-            if (node == dst) return cost;
-            if (stops > k) continue;
+            for(int[] f : flights) {
+                int u = f[0], v = f[1], w = f[2];
 
-            for (int[] e : adj.get(node)) {
-                int nei = e[0], price = e[1];
-                if (cost + price < dist[nei][stops+1]) {
-                    dist[nei][stops+1] = cost + price;
-                    pq.add(new int[]{nei, cost+price, stops+1});
+                if(dist[u] + w < temp[v]) {
+                    temp[v] = dist[u] + w;
                 }
             }
+
+            dist = temp;
         }
-        return -1;
+
+        return dist[dst] != INF ? dist[dst] : -1;
     }
 }
