5945ae6b2fd5419d461fd0c53867ac50338cb796
Abinash <110354418+Abinash6000@users.noreply.github.com>
2025-09-04 18:01:07 +0530

    Time: 0 ms (100%), Space: 41.8 MB (27.52%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,35 +1,20 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        morrisTraversal(root, list);
-        return list;
-    }
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
 
-    private void morrisTraversal(TreeNode root, List<Integer> list) {
-        TreeNode curr, pre; // current and predecessor
-        curr = root;
-        while(curr != null) {
-
-            if(curr.left == null) {
-
-                list.add(curr.val);
-                curr = curr.right;
-
-            } else {
-
-                pre = curr.left;
-                while(pre.right != curr && pre.right != null) 
-                    pre = pre.right;
-
-                if(pre.right == null) {
-                    pre.right = curr; // create the thread
-                    curr = curr.left;
-                } else { // pre.right == curr
-                    pre.right = null; // remove the thread
-                    list.add(curr.val);
-                    curr = curr.right;
-                }
+        while (root != null || !st.isEmpty()) {
+            // Go to the leftmost node
+            while (root != null) {
+                st.push(root);
+                root = root.left;
             }
+
+            root = st.pop();          // process node
+            res.add(root.val);
+
+            root = root.right;        // move to right subtree
         }
+        return res;
     }
 }
