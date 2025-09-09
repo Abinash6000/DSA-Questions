868bad694ae5e4dfb8f5dc40cd43fce4899e5bd7
Abinash <110354418+Abinash6000@users.noreply.github.com>
2025-09-09 08:12:35 +0530

    Time: 48 ms (69.01%), Space: 48.2 MB (7.96%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,35 +1,29 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return subAWKD(nums, k) - subAWKD(nums, k-1);
+        return subsWithK(nums, k) - subsWithK(nums, k-1);
     }
 
-    private int subAWKD(int[] nums, int k) {
-        int st = 0, ed = 0, n = nums.length;
-        int[] map = new int[n+1]; // num, count
-        int count = 0; // count k or lesser diff ints
-        int noOfEle = 0;
+    private int subsWithK(int[] nums, int k) {
+        int res = 0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int st = 0, ed = 0;
 
-        while(ed < n) {
-            map[nums[ed]]++;
-            if(map[nums[ed]] == 1) noOfEle++;
+        while(ed < nums.length) {
+            hm.put(nums[ed], hm.getOrDefault(nums[ed], 0)+1);
 
-            while(noOfEle > k) {
-                count += ed - st;
-                map[nums[st]]--;
+            while(hm.size() > k) {
+                hm.put(nums[st], hm.get(nums[st]) -1);
+                if(hm.get(nums[st]) == 0)
+                    hm.remove(nums[st]);
 
-                if(map[nums[st]] == 0) noOfEle--;
+                
                 st++;
             }
 
+            res += ed-st+1;
             ed++;
         }
 
-        // add remaining subArrs from st to ed
-        while(st < n) {
-            count += ed - st;
-            st++;
-        }
-
-        return count;
+        return res;
     }
 }
