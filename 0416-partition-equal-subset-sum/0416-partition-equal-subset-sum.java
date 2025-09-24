9ac90d7e903f993126bd5344b2fffeeb57c941f0
Abinash <110354418+Abinash6000@users.noreply.github.com>
2025-09-24 13:28:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,40 +1,26 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int i = 0; i<nums.length; i++) sum+= nums[i];
-        boolean prev[] = new boolean[sum/2+1];
-        boolean curr[] = new boolean[sum/2+1];
-        if(sum%2 != 0) return false;
+        for(int i = 0; i<nums.length; i++) sum += nums[i];
+
+        if(sum %2 != 0) return false;
         sum = sum/2;
 
-        for(int j = 0; j<=sum; j++) prev[j] = (nums[0] == j ? true : false);
-
-        for(int i = 1; i<nums.length; i++) {
-            for(int j = 1; j<=sum; j++) {
-                if(nums[i] <= j) {
-                    curr[j] = prev[j-nums[i]] || prev[j];
-                } else {
-                    curr[j] = prev[j];
-                }
-            }
-            prev = curr.clone();
-        }
-
-        return curr[sum];
+        return canPart(nums, sum, 0);
     }
 
-    // private boolean helper(int nums[], int i, int sum, Boolean memo[][]) {
-    //     if(sum == 0) return true;
-    //     if(i == nums.length) return false;
-    //     if(memo[i][sum] != null) return memo[i][sum];
+    private boolean canPart(int[] nums, int sum, int i) {
+        if(i == nums.length)
+            return false;
 
-    //     if(nums[i] <= sum) {
-    //         // two choices
-    //         return memo[i][sum] = (
-    //             helper(nums, i+1, sum-nums[i], memo)
-    //             || helper(nums, i+1, sum, memo));
-    //     } else {
-    //         return memo[i][sum] = helper(nums, i+1, sum, memo);
-    //     }
-    // }
+        if(sum == 0)
+            return true;
+
+        boolean res = false;
+        if(sum - nums[i] >= 0)
+            res = canPart(nums, sum-nums[i], i+1);
+        res |= canPart(nums, sum, i+1);
+
+        return res;
+    }
 }
