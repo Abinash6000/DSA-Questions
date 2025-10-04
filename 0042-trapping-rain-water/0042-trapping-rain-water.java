6f597bfdacaa15dba8d85c6ab5eab52682913ce6
Abinash <110354418+Abinash6000@users.noreply.github.com>
2025-10-04 09:40:55 +0530

    Time: 6 ms (5.61%), Space: 45.4 MB (98.79%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int trap(int[] height) {
-        int n = height.length;
-        int[] preMax = new int[n], sufMax = new int[n];
-
-        for(int i = 1; i<n; i++) {
-            preMax[i] = Math.max(preMax[i-1], height[i-1]);
-            sufMax[n-i-1] = Math.max(sufMax[n-i], height[n-i]); 
+    public int trap(int[] A) {
+        if (A==null) return 0;
+        Stack<Integer> s = new Stack<Integer>();
+        int i = 0, maxWater = 0, maxBotWater = 0;
+        while (i < A.length){
+            if (s.isEmpty() || A[i]<=A[s.peek()]){
+                s.push(i++);
+            }
+            else {
+                int bot = s.pop();
+                maxBotWater = s.isEmpty()? // empty means no il
+                0:(Math.min(A[s.peek()],A[i])-A[bot])*(i-s.peek()-1);
+                maxWater += maxBotWater;
+            }
         }
-
-        int vol = 0;
-        for(int i = 0; i<n; i++) {
-            int curVol = Math.min(preMax[i], sufMax[i]) - height[i];
-            if(curVol > 0) vol += curVol;
-        }
-
-        return vol;
+        return maxWater;
     }
 }
