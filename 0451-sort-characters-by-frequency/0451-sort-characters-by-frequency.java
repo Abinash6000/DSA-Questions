ee70fc01f01078ac39814e63bfb1037c0a2a9839
Abinash <110354418+Abinash6000@users.noreply.github.com>
2025-10-07 14:11:28 +0530

    Time: 3 ms (99.54%), Space: 44.2 MB (99.92%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,43 +1,18 @@
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character, Integer> hm = new HashMap<>();
-
-        for(char c : s.toCharArray()) {
-            hm.put(c, hm.getOrDefault(c, 0) + 1);
-        }
-
-        Pair[] counts = new Pair[hm.size()];
-        int i = 0;
-        for(Map.Entry<Character, Integer> e: hm.entrySet()) {
-            counts[i++] = new Pair(e.getKey(), e.getValue());
-        }
-
-        Arrays.sort(counts, new CompareByValue());
-
-        StringBuilder res = new StringBuilder();
-        for(int j = 0; j<counts.length; j++) {
-            int currCount = counts[j].count;
-            char currLetter = counts[j].letter;
-            for(int k = 0; k<currCount; k++) {
-                res.append(currLetter);
+        char[] str=s.toCharArray();
+        int[] freq=new int[123];
+        for(int i=0;i<str.length;i++) freq[str[i]]++;
+        for(int i=0;i<str.length;){
+            char cmax=',';
+            for(int j=0;j<123;j++){
+                if(freq[j]>freq[cmax]) cmax=(char)j;
+            }
+            while(freq[cmax]!=0){
+                str[i++]=cmax;
+                freq[cmax]--;
             }
         }
-
-        return res.toString();
-    }
-}
-
-class CompareByValue implements Comparator<Pair> {
-    public int compare(Pair a, Pair b) {
-        return b.count - a.count;
-    }
-}
-
-class Pair {
-    char letter;
-    int count;
-    Pair(char letter, int count) {
-        this.letter = letter;
-        this.count = count;
+        return new String(str);
     }
 }
