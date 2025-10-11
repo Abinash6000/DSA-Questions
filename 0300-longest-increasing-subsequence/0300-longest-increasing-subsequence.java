9f3cd48f8d1afb93b081e2ad22fa134a39a662ab
Abinash <110354418+Abinash6000@users.noreply.github.com>
2025-10-11 06:34:10 +0530

    Time: 2 ms (99.65%), Space: 44.4 MB (58.94%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,21 +1,20 @@
 class Solution {
-    int memo[];
     public int lengthOfLIS(int[] nums) {
-        memo = new int[nums.length+1];
-        return helper(nums, -1, 0);
+    int[] tails = new int[nums.length];
+    int size = 0;
+    for (int x : nums) {
+        int i = 0, j = size;
+        while (i != j) {
+            int m = (i + j) / 2;
+            if (tails[m] < x)
+                i = m + 1;
+            else
+                j = m;
+        }
+        tails[i] = x;
+        if (i == size) ++size;
     }
-
-    private int helper(int[] nums, int prev_i, int i) {
-        if(i == nums.length) return 0;
-        if(memo[prev_i+1] != 0) return memo[prev_i+1];
-
-        int ss1 = 0;
-
-        if(prev_i == -1 || nums[i] > nums[prev_i])
-            ss1 = 1 + helper(nums, i, i+1); // select
-        
-        int ss2 = helper(nums, prev_i, i+1); // not select
-
-        return memo[prev_i+1] = Math.max(ss1, ss2);
-    }
+    return size;
+}
+// Runtime: 2 ms
 }
