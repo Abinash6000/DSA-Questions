97a3c2080860d9724409595120a101a2f17d875e
Abinash <110354418+Abinash6000@users.noreply.github.com>
2025-10-20 18:48:47 +0530

    Time: 3 ms (70.18%), Space: 43 MB (98.03%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -14,6 +14,7 @@ class Solution {
         if (target < 0) return;
         for (int i = cur; i < cand.length; i++){
             if (i > cur && cand[i] == cand[i-1]) continue;
+            if(cand[i] > target) break;
             path.add(path.size(), cand[i]);
             dfs_com(cand, i+1, target - cand[i], path, res);
             path.remove(path.size()-1);
