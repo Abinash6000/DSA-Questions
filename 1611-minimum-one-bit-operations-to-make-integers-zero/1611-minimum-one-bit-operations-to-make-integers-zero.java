6262071a9ceebc117994c7c2386366776c54e749
Abinash <110354418+Abinash6000@users.noreply.github.com>
2025-11-08 18:02:14 +0530

    Time: 1 ms (10%), Space: 42.2 MB (9%) - LeetHub

---
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumOneBitOperations(int n) {
+        if(n == 0)
+            return 0;
+
+        int k = 0;
+        while(Math.pow(2,k) <= n) {
+            k+= 1;
+        }
+        k--;
+
+        return (int)Math.pow(2,k+1) -1 -minimumOneBitOperations((int)Math.pow(2,k)^n);
+    }
+}
