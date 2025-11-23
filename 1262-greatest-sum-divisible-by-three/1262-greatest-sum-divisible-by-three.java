ddc3ab8a1be550dfa399e45f1d5391f4800d2e77
Abinash <110354418+Abinash6000@users.noreply.github.com>
2025-11-23 10:38:33 +0530

    Time: 16 ms (28.57%), Space: 55.4 MB (6.21%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
@@ -1,21 +1,28 @@
 class Solution {
-    public int maxSumDivThree(int[] nums) {
-        int n = nums.length;
-        Integer[][] mm = new Integer[n][3];
-        return solve(0, nums, 0, mm);
+
+    private int solve(int i, int[] nums, int r, int[][] dp) {
+        if (i == nums.length) {
+            return r == 0 ? 0 : -1000000;
+        }
+
+        if (dp[i][r] != -1) {
+            return dp[i][r];
+        }
+
+        int take = nums[i] + solve(i + 1, nums, (r + nums[i]) % 3, dp);
+        int skip = solve(i + 1, nums, r, dp);
+
+        return dp[i][r] = Math.max(take, skip);
     }
 
-    private int solve(int i, int[] nums, int r, Integer[][] mm) {
-        if(i == nums.length) {
-            if(r == 0)
-                return 0;
-            return -1000000;
+    public int maxSumDivThree(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][3];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        if(mm[i][r] != null) return mm[i][r];
 
-        int op1 = nums[i] + solve(i+1, nums, (r + nums[i]) % 3, mm);
-        int op2 = solve(i+1, nums, r, mm);
-
-        return mm[i][r] = Math.max(op1, op2);
+        return solve(0, nums, 0, dp);
     }
 }
