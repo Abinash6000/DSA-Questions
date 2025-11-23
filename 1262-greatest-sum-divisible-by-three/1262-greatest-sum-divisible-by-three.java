74e1acc15a7e2e17d1784d9bff57855c5ae12c2e
Abinash <110354418+Abinash6000@users.noreply.github.com>
2025-11-23 10:38:39 +0530

    Time: 27 ms (6.52%), Space: 50.9 MB (22.36%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
@@ -1,28 +1,26 @@
 class Solution {
 
-    private int solve(int i, int[] nums, int r, int[][] dp) {
-        if (i == nums.length) {
-            return r == 0 ? 0 : -1000000;
-        }
-
-        if (dp[i][r] != -1) {
-            return dp[i][r];
-        }
-
-        int take = nums[i] + solve(i + 1, nums, (r + nums[i]) % 3, dp);
-        int skip = solve(i + 1, nums, r, dp);
-
-        return dp[i][r] = Math.max(take, skip);
-    }
-
     public int maxSumDivThree(int[] nums) {
         int n = nums.length;
         int[][] dp = new int[n][3];
 
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
+        int mod = nums[0] % 3;
+        if (mod == 0) dp[0][0] = nums[0];
+        else if (mod == 1) dp[0][1] = nums[0];
+        else dp[0][2] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = dp[i - 1].clone();
+
+            int x = dp[i - 1][0] + nums[i];
+            int y = dp[i - 1][1] + nums[i];
+            int z = dp[i - 1][2] + nums[i];
+
+            dp[i][x % 3] = Math.max(dp[i][x % 3], x);
+            dp[i][y % 3] = Math.max(dp[i][y % 3], y);
+            dp[i][z % 3] = Math.max(dp[i][z % 3], z);
         }
 
-        return solve(0, nums, 0, dp);
+        return dp[n - 1][0];
     }
 }
