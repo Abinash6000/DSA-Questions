e3e4e6c5a31d1eabcbb5e4b1783c5f569b45ff08
Abinash <110354418+Abinash6000@users.noreply.github.com>
2025-12-25 21:02:33 +0530

    Time: 213 ms (5.38%), Space: 122.4 MB (6.45%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -1,27 +1,24 @@
 class Solution {
     public long maximumHappinessSum(int[] happiness, int k) {
-        int happinessSize = happiness.length;
-        
-        // Convert the array to an Integer array for sorting in descending order
-        Integer[] happinessArray = new Integer[happinessSize];
-        for(int i = 0; i < happinessSize; i++) {
-            happinessArray[i] = happiness[i];
+        // Create a max heap using PriorityQueue with a custom comparator
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        // Add all elements to the priority queue
+        for (int h : happiness) {
+            pq.add(h);
         }
-        
-        Arrays.sort(happinessArray, Collections.reverseOrder());
 
         long totalHappinessSum = 0;
         int turns = 0;
-        
-        // Calculate the maximum happiness sum
-        for(int i = 0; i < k; i++) {
-            // Adjust happiness and ensure it's not negative
-            totalHappinessSum += Math.max(happinessArray[i] - turns, 0);  
 
-             // Increment turns for the next iteration
-            turns++; 
+        for (int i = 0; i < k; i++) {
+            // Add the current highest value to the total happiness sum and remove it from the max heap 
+            totalHappinessSum += Math.max(pq.poll() - turns, 0);
+
+            // Increment turns for the next iteration
+            turns++;
         }
-        
+
         return totalHappinessSum;
     }
 }
