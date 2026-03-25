de06d2cc712f69bba5794217e6bfb5e0808fd8e5
Abinash <110354418+Abinash6000@users.noreply.github.com>
2026-03-25 20:33:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -1,30 +1,37 @@
 class Solution {
-    public boolean canPartitionGrid(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        long total = 0;
-        
-        for (int[] row : grid)
-            for (int x : row)
-                total += x;
-        
-        if ((total & 1) == 1) return false;
-        
-        long target = total / 2, sum = 0;
-        
-        for (int i = 0; i < m - 1; i++) {
-            for (int j = 0; j < n; j++)
-                sum += grid[i][j];
-            if (sum == target) return true;
+    static public boolean canPartitionGrid(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        long totalRowSum = 0L, totalColSum = 0L;
+        long[] prefixRowWise = new long[n];
+        long[] prefixColWise = new long[m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int v = grid[i][j];
+                long vl = v;
+                prefixRowWise[i] += vl;
+                prefixColWise[j] += vl;
+            }
         }
-        
-        sum = 0;
-        
-        for (int j = 0; j < n - 1; j++) {
-            for (int i = 0; i < m; i++)
-                sum += grid[i][j];
-            if (sum == target) return true;
+
+        for (long r : prefixRowWise) totalRowSum += r;
+        totalColSum = totalRowSum;
+
+        long currentRowUpperSum = 0L;
+        for (int i = 0; i < n - 1; i++) {
+            currentRowUpperSum += prefixRowWise[i];
+            long lowerSegmentSum = totalRowSum - currentRowUpperSum;
+            if (currentRowUpperSum == lowerSegmentSum) return true;
         }
-        
+
+        long currentColLeftSum = 0L;
+        for (int j = 0; j < m - 1; j++) {
+            currentColLeftSum += prefixColWise[j];
+            long rightSegmentSum = totalColSum - currentColLeftSum;
+            if (currentColLeftSum == rightSegmentSum) return true;
+        }
         return false;
     }
 }
