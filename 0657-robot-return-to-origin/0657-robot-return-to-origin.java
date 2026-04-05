8f0e9f2e4f8efa6d083ed839dae1ff440fddd1ae
Abinash <110354418+Abinash6000@users.noreply.github.com>
2026-04-05 06:51:55 +0530

    Time: 5 ms (68.18%), Space: 46.1 MB (42.38%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int up = 0, right = 0;
+        for(char c : moves.toCharArray()) {
+            if(c == 'U')
+                up++;
+            else if(c == 'R')
+                right++;
+            else if(c == 'D')
+                up--;
+            else
+                right--;
+        }
+
+        return up == 0 && right == 0;
+    }
+}
